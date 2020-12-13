fd286aa90dfe2705371ed6f06089e0a87fa232f1
DavidTu <a3102a123@gamil.com>
2020-12-13 17:12:40 +0800

    add CUDA function

--- kernel.cu
+++ kernel.cu
@@ -2,16 +2,50 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-__global__ void mandelKernel() {
+__global__ void mandelKernel(float lowerX, float lowerY, float stepX, float stepY,int width,int count, int *output) {
     // To avoid error caused by the floating number, use the following pseudo code
     //
     // float x = lowerX + thisX * stepX;
     // float y = lowerY + thisY * stepY;
+    int i = threadIdx.x;
+    int j = threadIdx.y;
+    float x = lowerX + thisX * stepX;
+    float y = lowerY + thisY * stepY;
+
+    int idx;
+    float z_re = x, z_im = y;
+    for (idx = 0; idx < count; ++idx)
+    {
+
+        if (z_re * z_re + z_im * z_im > 4.f)
+        break;
+
+        float new_re = z_re * z_re - z_im * z_im;
+        float new_im = 2.f * z_re * z_im;
+        z_re = x + new_re;
+        z_im = y + new_im;
+    }
+
+    int index = (j * width + i);
+    output[index] = idx;
 }
 
 // Host front-end function that allocates the memory and launches the GPU kernel
 void hostFE (float upperX, float upperY, float lowerX, float lowerY, int* img, int resX, int resY, int maxIterations)
 {
+    int size = resX * resY;
     float stepX = (upperX - lowerX) / resX;
     float stepY = (upperY - lowerY) / resY;
+    // allocate memory in host & device
+    int *host_mem, *dev_mem;
+    host_mem = (int *)malloc(size * sizeof(int));
+    cudaMalloc((void **)&dev_mem, size * sizeof(int));
+    // GPU processing 
+    dim3 threadsPerBlock(resX, resY);
+    mandelKernel<<<1, threadsPerBlock>>>(lowerX, lowerY, stepX, stepY, resX, maxIterations, dev_mem);
+    cudaDeviceSynchronize();
+    // GPU translate result data back
+    cudaMemcpy(host_mem, dev_mem, size * sizeof(int), cudaMemcpyDeviceToHost);
+    free(host_mem);
+    cudaFree(dev_mem);
 }
