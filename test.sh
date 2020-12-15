61d6412fb174d2f4b262de9a121135c5c6ba1657
DavidTu <a3102a123@gamil.com>
2020-12-15 20:24:25 +0800

    add check in sh

--- test.sh
+++ test.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 cp ${1} kernel.cu
-if [[ 0 == $(make) ]] ; do
-    echo "Make Success!"
-done
-# for j in {1..2} ; do
-#     for i in 1000 10000 100000 ; do
-#         ./mandelbrot -g 1 -v ${j} -i ${i}
-#     done
-# done    
+rm mandelbrot
+make
+if [ `ls | grep mandelbrot$` ] ; then
+    for j in {1..2} ; do
+        for i in 1000 10000 100000 ; do
+            ./mandelbrot -g 1 -v ${j} -i ${i}
+        done
+    done
+fi
