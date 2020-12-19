e7c2279dc45c0bf1d515cf3b05087a75e85e56fd
DavidTu <a3102a123@gamil.com>
2020-12-19 14:16:09 +0000

    fix v4

--- kernel4.cu
+++ kernel4.cu
@@ -14,9 +14,7 @@ __global__ void mandelKernel(float lowerX, float lowerY, float stepX, float step
     int j = blockIdx.y * blockDim.y + threadIdx.y;
     int index = (j * width + i);
     float c_re = lowerX + i * stepX;
-    float c_im = lowerY + j * stepY;
-
-    i += round * round_size; 
+    float c_im = lowerY + (j + round * round_size) * stepY;
 
     int idx;
     float z_re = c_re, z_im = c_im;
@@ -39,11 +37,11 @@ __global__ void mandelKernel(float lowerX, float lowerY, float stepX, float step
 void hostFE (float upperX, float upperY, float lowerX, float lowerY, int* img, int resX, int resY, int maxIterations)
 {
     int size = resX * resY * sizeof(int);
-    int round_size = resX / XBLOCK_SIZE * resY / YBLOCK_SIZE;
+    int round_size = resX * YBLOCK_SIZE * sizeof(int);
     float stepX = (upperX - lowerX) / resX;
     float stepY = (upperY - lowerY) / resY;
     // allocate memory in host & device
-    int *host_mem, *host_temp_mem, *dev_ptr, *dev_pre_ptr, *dev_mem1, *dev_mem2;
+    int *host_mem, *host_temp_mem, *dev_ptr, *dev_pre_ptr, *dev_mem1, *dev_mem2,*temp_ptr;
     host_mem = (int *)malloc(size);
     cudaHostAlloc(&host_temp_mem, size, cudaHostAllocDefault);
     cudaMalloc((void **)&dev_mem1, round_size);
@@ -51,13 +49,22 @@ void hostFE (float upperX, float upperY, float lowerX, float lowerY, int* img, i
     dev_ptr = dev_mem1;
     dev_pre_ptr = dev_mem2;
     // GPU processing 
-    dim3 num_block(resX / XBLOCK_SIZE, resY / YBLOCK_SIZE);
-    dim3 block_size(1, YBLOCK_SIZE);
-    for(int i = 0 ; i < XBLOCK_SIZE ; i++){
-        mandelKernel<<<num_block, block_size>>>(lowerX, lowerY, stepX, stepY, resX, maxIterations, dev_mem1, i, round_size);
+    dim3 num_block(resX / XBLOCK_SIZE, 1);
+    dim3 block_size(XBLOCK_SIZE, YBLOCK_SIZE);
+    for(int j = 0 ; j < resY / YBLOCK_SIZE ; j++){
+        mandelKernel<<<num_block, block_size>>>(lowerX, lowerY, stepX, stepY, resX, maxIterations, dev_ptr, j, YBLOCK_SIZE);
         cudaDeviceSynchronize();
-        cudaMemcpy(host_mem + i * round_size, dev_mem1, round_size, cudaMemcpyDeviceToHost);
+        temp_ptr = dev_ptr;
+        dev_ptr = dev_pre_ptr;
+        dev_pre_ptr = temp_ptr;
+        temp_ptr = host_mem + (round_size / sizeof(int)) * j;
+        cudaMemcpy( temp_ptr, dev_pre_ptr, round_size, cudaMemcpyDeviceToHost);
     }
+    /*for(int j = 25 ; j < 28 ; j++){
+        for(int i = 0 ; i < resX ; i++)
+            printf("%d ",host_mem[i + j * resX]);
+        printf("\n");
+    }*/
     // GPU translate result data back
     memcpy(img, host_mem, size);
     cudaFreeHost(host_mem);
